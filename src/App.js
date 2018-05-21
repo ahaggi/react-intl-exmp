cd8fc7cb9f69679b281402c91a8538a94a6c6d9a
ahaggi <a.haggi@yahoo.com>
2018-05-21 09:52:48 +0200

    qwe

--- src/App.js
+++ src/App.js
@@ -17,41 +17,55 @@ class App extends Component {
     addLocaleData(require('react-intl/locale-data/nb'));
   }
 
+renderElements = () => (
+  <IntlProvider key={this.state.locale.code} locale={this.state.locale.code} messages={this.state.locale.messages}>
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+
+    <button onClick={()=> this.setState({locale:nbBundle})}>no</button>
+    <button onClick={()=> this.setState({locale:enBundle})}>en</button>
+
+      <p><FormattedMessage id={"app.title"} /></p>
+      <p><FormattedMessage id={"app.heading"} /></p>
+      <p><FormattedMessage id={"app.language"} /></p>
+
+      {
+        Object.keys(this.state.locale).map((k)=> (<p>{k}  : 
+                                                    {typeof this.state.locale[k]=== "string" ? this.state.locale[k]: 'noe noe'}
+                                                    </p>))
+      }
+    </div>
+  </IntlProvider >
+
+);
 
   render() {
     console.log(nbBundle)
     console.log(enBundle)
 
-    return (
-      <IntlProvider key={this.state.locale.code} locale={this.state.locale.code} messages={this.state.locale.messages}>
 
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+    if (!window.Intl) {
+      require.ensure([
+        'intl',
+        'intl/locale-data/jsonp/'+this.state.locale.code+'.js'
+      ], 
+      (require) => {
+        require('intl');
+        require('intl/locale-data/jsonp/'+this.state.locale.code+'.js');
+        return this.renderElements()
+      });
+    } else {
+      return this.renderElements()
+    }
 
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <button onClick={()=> this.setState({locale:nbBundle})}>no</button>
-        <button onClick={()=> this.setState({locale:enBundle})}>en</button>
-
-          <p><FormattedMessage id={"app.title"} /></p>
-          <p><FormattedMessage id={"app.heading"} /></p>
-          <p><FormattedMessage id={"app.language"} /></p>
-
-          {
-            Object.keys(this.state.locale).map((k)=> (<p>{k}  : 
-                                                        
-                                                        {typeof this.state.locale[k]=== "string" ? this.state.locale[k]: 'noe noe'}
-                                                        </p>))
-          }
-        </div>
-      </IntlProvider >
-
-    );
   }
 }
 
